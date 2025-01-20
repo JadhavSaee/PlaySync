2e603fdbcec65d56a4db63992915522b7b441828
JadhavSaee <157830145+JadhavSaee@users.noreply.github.com>
2025-01-20 23:16:49 +0530

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const colors = {
+    primary: '#00FFFF', 
+    background: '#121212', 
+    text: '#FFFFFF', 
+    buttonText: '#121212', 
+  };
+  
+  export const fonts = {
+    heading: 'Roboto-Bold',
+    body: 'Roboto-Regular',
+  };
+  
