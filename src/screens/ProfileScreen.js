1e56688418d3569ba59d6f12157141c974c44047
JadhavSaee <157830145+JadhavSaee@users.noreply.github.com>
2025-01-20 23:12:25 +0530

    Create ProfileScreen.js

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ProfileScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is your Profile in PlaySync!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#00FFFF',
+    fontSize: 20,
+  },
+});
+
+export default ProfileScreen;
