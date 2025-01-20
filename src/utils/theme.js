0aacb05fd81b4c9fe5f2d52195aedd1412b46973
JadhavSaee <157830145+JadhavSaee@users.noreply.github.com>
2025-01-20 23:14:12 +0530

    Create theme.js

---
+++ src/utils/theme.js
@@ -0,0 +1,25 @@
+export const colors = {
+    primary: '#0d7377',
+    secondary: '#14ffec',
+    background: '#212121',
+    text: '#eeeeee',
+    error: '#ff5252',
+  };
+  
+  export const fonts = {
+    regular: 'Roboto-Regular',
+    bold: 'Roboto-Bold',
+  };
+  
+  export const styles = {
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+      padding: 16,
+    },
+    text: {
+      color: colors.text,
+      fontFamily: fonts.regular,
+    },
+  };
+  
