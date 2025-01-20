29b56f4a7036c534089d9b939ed1ad8f4da00adc
JadhavSaee <157830145+JadhavSaee@users.noreply.github.com>
2025-01-20 23:10:02 +0530

    Create HomeScreen.js

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to the Home Screen of PlaySync!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#00FFFF',
+    fontSize: 20,
+  },
+});
+
+export default HomeScreen;
